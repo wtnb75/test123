df1605d5e0ff48c33bf17e65284d2ba7b69c922d
Watanabe AI Takashi <wtnb75@gmail.com>
2026-08-16 18:31:12 +0900

    cjump: wire all scenes into the game config

--- cjump/src/game/main.ts
+++ cjump/src/game/main.ts
@@ -1,25 +1,26 @@
-import { Game as MainGame } from './scenes/Game';
-import { AUTO, Game, Scale,Types } from 'phaser';
+import { AUTO, Game, Scale, type Types } from 'phaser';
+import { Boot } from './scenes/Boot';
+import { StageSelect } from './scenes/StageSelect';
+import { Game as GameScene } from './scenes/Game';
+import { StageClear } from './scenes/StageClear';
+import { GameOver } from './scenes/GameOver';
+import { GAME_WIDTH, GAME_HEIGHT } from './logic/constants';
 
-// Find out more information about the Game Config at:
-// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#16213e',
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
     },
-    scene: [
-        MainGame
-    ]
+    scene: [Boot, StageSelect, GameScene, StageClear, GameOver]
 };
 
 const StartGame = (parent: string) => {
     return new Game({ ...config, parent });
-}
+};
 
 export default StartGame;
